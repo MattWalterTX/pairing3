7d7955a719ae08a0fb23b96c62ae44fe5523a59f
Matt Walter <matt.walter.tx@outlook.com>
2022-06-09 16:42:17 -0500

    Add new code

--- friends.js
+++ friends.js
@@ -4,3 +4,12 @@ for (var i = 0; i < num.length; i++) {
   if (num [i] > 30)
   console.log(num[i])
 }
+
+console.log("================")
+
+num = [9, 16, 33, 55, 86, 89, 92, 97]
+
+for (var i = 0; i < num.length; i++) {
+  if (num [i] < 60)
+  console.log(num[i])
+}
